2f87a5a9444e0e2bb5f60262db77b30b2f94e3e4
Mark Pittillo <mark-pittillo@uiowa.edu>
2013-06-04 23:00:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fsmc::Application.config.secret_key_base = 'd932382417ed769eebeec7078e56bb4fcf099b89f237d4433418f5edc172c8884ed1e7d82e4863009dbf715b5383244130f8178ca354c8c9db0d5080ab7a6632'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fsmc::Application.config.session_store :cookie_store, key: '_fsmc_session'
